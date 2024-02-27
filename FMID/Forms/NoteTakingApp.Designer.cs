7352791672c46d53dfaed00dba83d12987c4442f
Brandon McKinney <brandonaustinmckinney808@gmail.com>
2024-02-27 10:02:49 -0500

    Add project files.

---
+++ FMID/Forms/NoteTakingApp.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace FMID
+{
+    partial class NotesAppForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotesAppForm));
+            this.barManager1 = new DevExpress.XtraBars.BarManager(this.components);
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.fileMenu = new DevExpress.XtraBars.BarSubItem();
+            this.openTextFileButton = new DevExpress.XtraBars.BarButtonItem();
+            this.saveTextFileButton = new DevExpress.XtraBars.BarButtonItem();
+            this.saveAsButton = new DevExpress.XtraBars.BarButtonItem();
+            this.barSubItem2 = new DevExpress.XtraBars.BarSubItem();
+            this.barButtonItem4 = new DevExpress.XtraBars.BarButtonItem();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+            this.noteBox = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.infoLabel = new DevExpress.XtraBars.BarStaticItem();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar2,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.fileMenu,
+            this.saveTextFileButton,
+            this.barButtonItem2,
+            this.openTextFileButton,
+            this.barSubItem2,
+            this.barButtonItem4,
+            this.saveAsButton,
+            this.infoLabel});
+            this.barManager1.MainMenu = this.bar2;
+            this.barManager1.MaxItemId = 9;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Main menu";
+            this.bar2.DockCol = 0;
+            this.bar2.DockRow = 0;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.fileMenu),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barSubItem2)});
+            this.bar2.OptionsBar.AllowQuickCustomization = false;
+            this.bar2.OptionsBar.DrawBorder = false;
+            this.bar2.OptionsBar.DrawDragBorder = false;
+            this.bar2.OptionsBar.MultiLine = true;
+            this.bar2.OptionsBar.UseWholeRow = true;
+            this.bar2.Text = "Main menu";
+            // 
+            // fileMenu
+            // 
+            this.fileMenu.Caption = "File";
+            this.fileMenu.Id = 1;
+            this.fileMenu.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.openTextFileButton),
+            new DevExpress.XtraBars.LinkPersistInfo(this.saveTextFileButton),
+            new DevExpress.XtraBars.LinkPersistInfo(this.saveAsButton)});
+            this.fileMenu.Name = "fileMenu";
+            // 
+            // openTextFileButton
+            // 
+            this.openTextFileButton.Caption = "Open";
+            this.openTextFileButton.Id = 4;
+            this.openTextFileButton.Name = "openTextFileButton";
+            this.openTextFileButton.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.openTextFileButton_ItemClick);
+            // 
+            // saveTextFileButton
+            // 
+            this.saveTextFileButton.Caption = "Save";
+            this.saveTextFileButton.Id = 2;
+            this.saveTextFileButton.Name = "saveTextFileButton";
+            this.saveTextFileButton.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.saveTextFileButton_ItemClick);
+            // 
+            // saveAsButton
+            // 
+            this.saveAsButton.Caption = "Save As";
+            this.saveAsButton.Id = 7;
+            this.saveAsButton.Name = "saveAsButton";
+            this.saveAsButton.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.saveAsButton_ItemClick);
+            // 
+            // barSubItem2
+            // 
+            this.barSubItem2.Caption = "Edit";
+            this.barSubItem2.Id = 5;
+            this.barSubItem2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem4)});
+            this.barSubItem2.Name = "barSubItem2";
+            // 
+            // barButtonItem4
+            // 
+            this.barButtonItem4.Caption = "Clear";
+            this.barButtonItem4.Id = 6;
+            this.barButtonItem4.Name = "barButtonItem4";
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "Status bar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockRow = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.infoLabel)});
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "Status bar";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Manager = this.barManager1;
+            this.barDockControlTop.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.barDockControlTop.Size = new System.Drawing.Size(981, 37);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 530);
+            this.barDockControlBottom.Manager = this.barManager1;
+            this.barDockControlBottom.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.barDockControlBottom.Size = new System.Drawing.Size(981, 37);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 37);
+            this.barDockControlLeft.Manager = this.barManager1;
+            this.barDockControlLeft.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 493);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(981, 37);
+            this.barDockControlRight.Manager = this.barManager1;
+            this.barDockControlRight.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 493);
+            // 
+            // barButtonItem2
+            // 
+            this.barButtonItem2.Caption = "Clear";
+            this.barButtonItem2.Id = 3;
+            this.barButtonItem2.Name = "barButtonItem2";
+            // 
+            // noteBox
+            // 
+            this.noteBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(44)))), ((int)(((byte)(44)))));
+            this.noteBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.noteBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.noteBox.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.noteBox.ForeColor = System.Drawing.Color.White;
+            this.noteBox.Location = new System.Drawing.Point(0, 37);
+            this.noteBox.Name = "noteBox";
+            this.noteBox.Size = new System.Drawing.Size(981, 493);
+            this.noteBox.TabIndex = 4;
+            this.noteBox.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Border = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.infoLabel.Id = 8;
+            this.infoLabel.Name = "infoLabel";
+            // 
+            // NotesAppForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(981, 567);
+            this.Controls.Add(this.noteBox);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.IconOptions.SvgImage = ((DevExpress.Utils.Svg.SvgImage)(resources.GetObject("NotesAppForm.IconOptions.SvgImage")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "NotesAppForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Notes App";
+            this.Load += new System.EventHandler(this.NotesAppForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarSubItem fileMenu;
+        private DevExpress.XtraBars.BarButtonItem openTextFileButton;
+        private DevExpress.XtraBars.BarButtonItem saveTextFileButton;
+        private DevExpress.XtraBars.BarSubItem barSubItem2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem4;
+        private System.Windows.Forms.RichTextBox noteBox;
+        private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private DevExpress.XtraBars.BarButtonItem saveAsButton;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private DevExpress.XtraBars.BarStaticItem infoLabel;
+    }
+}
+
---
+++ FMID/Forms/NoteTakingApp.cs
@@ -0,0 +1,87 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FMID
+{
+    public partial class NotesAppForm : DevExpress.XtraEditors.XtraForm
+    {
+        public NotesAppForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void openTextFileButton_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+
+            string textFile = openFileDialog1.FileName;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Make sure a file was selected
+                    if ((openFileDialog1.OpenFile()) != null)
+                    {
+                        // Open stream
+                        using (StreamReader sr = File.OpenText(openFileDialog1.FileName))
+                        {
+                            // Read the text
+                            noteBox.Text = sr.ReadToEnd();
+
+                            infoLabel.Caption = "File: " + openFileDialog1.FileName;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    XtraMessageBox.Show("An has error occured: " + ex.Message);
+                }
+            }
+        }
+
+        private void saveTextFileButton_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+
+        }
+
+        private void saveAsButton_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            string savedtext = noteBox.Text;
+
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(saveFileDialog1.FileName, noteBox.Text);
+
+                infoLabel.Caption = "File saved to: " + saveFileDialog1.FileName;
+            }
+            else
+            {
+                Exception ex = new Exception();
+                XtraMessageBox.Show("An has error occured: " + ex.Message);
+            }
+        }
+
+        private void NotesAppForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
